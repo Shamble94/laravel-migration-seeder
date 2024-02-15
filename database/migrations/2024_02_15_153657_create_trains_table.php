b4b6746ce50bd463959b7ea42d513877cb84cb04
Shamble94 <fratus.nicholas@gmail.com>
2024-02-15 17:05:41 +0100

    Ricreata tabella train per alcuni problemi

--- database/migrations/2024_02_15_153657_create_trains_table.php
+++ database/migrations/2024_02_15_153657_create_trains_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create("trains", function (Blueprint $train){
+        Schema::create("train", function (Blueprint $train){
             $train->id();
             $train->string("azienza", 30);
             $train->string("departure_station", 50);
             $train->string("destination", 50);
-            $train->smallInteger("departure_hour");
+            $train->string("date", 50);
+            $train->smallInteger("departure_hour"); 
             $train->smallInteger("arrival_hour");
             $train->string("train_code", 10);
             $train->tinyInteger("number_wagon")->unsigned();
@@ -34,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists("train");
     }
 };
---
+++ database/seeders/Train_seeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+class Train_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $trains = [
+            [
+                "azienda" => "Italo",
+                "departure_station" => "Roma Termini",
+                "destination" => "Milano Centrale"
+                "azienda" => ""
+                "azienda" => 
+                "azienda" =>
+                "azienda" =>
+                "azienda" =>
+                "azienda" =>
+                ]
+        ]
+    }
+}
